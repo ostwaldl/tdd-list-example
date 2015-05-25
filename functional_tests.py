8196de8064ece6ab846f708276a9056d1b231104
Leo <leo@rockville.home>
2015-05-25 15:36:50 -0400

    refactored table check

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app.  She goes
         # to check out its homepage
@@ -36,29 +41,24 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page Lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+ 
         # There is still a text box inviting her to ad another item.  She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical).
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        
-        # time.sleep(10)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
-        
-        # There is still a text box inviting her to add another item.  She
-        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('Finish the test!')
-        
-        # The page updates again, and now shows both items on her List
 
-        # Edit wonders whether the site will remember her List.  Then she sees
+        # The page updates again, and now shows both items on her List
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # Edith wonders whether the site will remember her List.  Then she sees
         # that the site has generatered a unique URL for her -- there is some
         # explanatory text to that effect.
+
+        self.fail('Finish the test!')
 
         # She visits that URL - her to-do List is still there.
 
