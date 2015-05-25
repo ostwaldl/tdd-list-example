35a23380e594d55aefadcec6913ed4cc746eefb1
Leo <leo@rockville.home>
2015-05-25 18:00:39 -0400

    added unit tests through checking redirect

--- lists/tests.py
+++ lists/tests.py
@@ -34,6 +34,11 @@ class ItemModelTest(TestCase):
                          
 
 class HomePageTest(TestCase):
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
     
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -52,12 +57,26 @@ class HomePageTest(TestCase):
 
         response = home_page(request)
 
-        self.assertIn('A new list item', response.content.decode())
-        expected_html = render_to_string(
-                'home.html',
-                {'new_item_text': 'A new list item'}
-                )
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+        
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+        
+        # self.assertIn('A new list item', response.content.decode())
+        #expected_html = render_to_string(
+        #        'home.html',
+        #        {'new_item_text': 'A new list item'}
+        #        )
+        #self.assertEqual(response.content.decode(), expected_html)
         
 
     
