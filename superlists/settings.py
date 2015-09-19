ce6a9fb360a0893e8747769a1f64ad755f174573
Leo <ostwaldl@gmail.com>
2015-09-19 13:28:05 -0400

    move sqlite database outside of main source tree

--- superlists/settings.py
+++ superlists/settings.py
@@ -102,3 +102,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
+        }
+}
+
